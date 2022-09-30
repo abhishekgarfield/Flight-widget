9a975f6301daaf16ea717090ee3dfe02d3744076
abhishekgarfield <abhishek2759@gmail.com>
2022-09-30 12:31:53 +0530

    working

--- src/Pages/Homescreen.js
+++ src/Pages/Homescreen.js
@@ -1,7 +1,65 @@
+import { toBeDisabled } from "@testing-library/jest-dom/dist/matchers";
+import { useEffect, useState } from "react";
+
 export const Homescreen = () => {
+  useEffect(() => {
+    var d = new Date();
+    var starttime = (
+      new Date(d.getFullYear(), d.getMonth(), d.getDate()).getTime() / 1000
+    ).toFixed(0);
+    var endtime = (
+      new Date(d.getFullYear(), d.getMonth(), d.getDate() + 1).getTime() / 1000
+    ).toFixed(0);
+    console.log(`start: ${starttime} ,end:${endtime}`);
+    const url = `https://opensky-network.org/api/flights/departure?airport=EDDF&begin=${starttime}
+        &end=${endtime}`;
+    fetch(url, {
+      method: "Get",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        display(data.slice(0, 6));
+      });
+  }, []);
+  function display(flights) {
+    const table = document.getElementsByTagName("table");
+    /* flights.forEach(flight => {
+            console.log(flight);
+        });
+        */
+
+    for (const flight of flights) {
+      console.log(flight);
+      var tablerow = document.createElement("tr");
+      var rowicon = document.createElement("td");
+      rowicon.textContent = "✈";
+      tablerow.append(rowicon);
+      const temp = {
+        callsign: flight.callsign,
+        destination: flight.estArrivalAirport,
+        pickup: flight.estDepartureAirport,
+        distance: flight.estDepartureAirportHorizDistance,
+        lastseen: flight.lastSeen,
+      };
+      for (const key in flight) {
+        console.log(flight[key]);
+        var el = document.createElement("td");
+        el.textContent = flight[key];
+        tablerow.appendChild(el);
+      }
+      table[0].appendChild(tablerow);
+    }
+  }
+
   return (
     <div className="First-container">
-      <div>DEPARTURES</div>
+      <div className="heading">
+        <h1>DEPARTURES</h1>
+      </div>
       <table>
         <tr>
           <th></th>
